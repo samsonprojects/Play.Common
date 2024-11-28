83b6ba4fd2fa737059106735a50c9084f040a929
samson Kwame <samson.antwi123@gmail.com>
2024-11-28 21:26:20 +0000

    added cosmos db support

--- README.md
+++ README.md
@@ -5,8 +5,13 @@ Common libraries used by Play microservices
 ## Create and publish package
 
 ```
-version="1.0.6"
+version="1.0.7"
 owner="samsonprojects"
+gh_pat="[PAT HERE]"
 
 dotnet pack src/Play.Common/ --configuration Release -p:PackageVersion=$version -p:RepositoryUrl=https://github.com/$owner/Play.Common -o ../packages
+
+
+dotnet nuget push ../packages/Play.Common.$version.nupkg --api-key $gh_pat --source "github"
+
 ```
--- src/Play.Common/Settings/MongoDbSettings.cs
+++ src/Play.Common/Settings/MongoDbSettings.cs
@@ -7,9 +7,19 @@ namespace Play.Common.Service.Settings
 {
     public class MongoDbSettings
     {
+        private string connectionString;
         public string Host { get; set; }
         public int Port { get; set; }
 
-        public string ConnectionString => $"mongodb://{Host}:{Port}";
+        public string ConnectionString
+        {
+            get
+            {
+                return string.IsNullOrWhiteSpace(connectionString)
+                    ? $"mongodb://{Host}:{Port}" : connectionString;
+            }
+            init { connectionString = value; }
+        }
+
     }
 }
